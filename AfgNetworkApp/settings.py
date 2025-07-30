1acc849b536455f705152ffb360d609c02f3f966
Nasrat Nasrati <115137645+Nasrat-Nasrati@users.noreply.github.com>
2025-07-30 15:14:29 +0430

    added the allowed host

--- AfgNetworkApp/settings.py
+++ AfgNetworkApp/settings.py
@@ -122,7 +122,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # # used for showing data to the api 
 # ALLOWED_HOSTS = ['172.16.3.236', 'localhost', '127.0.0.1']
 
-ALLOWED_HOSTS = ['my-django-backend.onrender.com']
+ALLOWED_HOSTS = ['afgnetworkapp.onrender.com', 'www.afgnetworkapp.onrender.com']
+
 
 
  #my custom reset framwork

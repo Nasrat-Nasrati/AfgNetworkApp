58ca2cc67645321da6273500cb2ae605bb283e65
Nasrat Nasrati <115137645+Nasrat-Nasrati@users.noreply.github.com>
2025-07-30 14:34:25 +0430

    added the allowed host of render.com

--- AfgNetworkApp/settings.py
+++ AfgNetworkApp/settings.py
@@ -117,8 +117,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# used for showing data to the api 
-ALLOWED_HOSTS = ['172.16.3.236', 'localhost', '127.0.0.1']
+# # used for showing data to the api 
+# ALLOWED_HOSTS = ['172.16.3.236', 'localhost', '127.0.0.1']
+
+ALLOWED_HOSTS = ['my-django-backend.onrender.com']
+
 
  #my custom reset framwork
 REST_FRAMEWORK = {
